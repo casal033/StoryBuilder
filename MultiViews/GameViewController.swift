63b6b7900404d7316c0b74a403fa35ec820491ee
Administrator <admin@x-146-57-20-28.uofm-secure.wireless.umn.edu>
2015-06-15 10:10:22 -0500

    switching to the heroic URL for getting information from Word River

--- MultiViews/GameViewController.swift
+++ MultiViews/GameViewController.swift
@@ -210,8 +210,8 @@ class GameViewController: UIViewController {
     }
     
     //var _words:[[String]] = WordList(arr: DEFAULT_WORD_LIST).wordsWithCategories
-    //var _words:[String] = WordList(url: "https://teacherwordriver.herokuapp.com/api/tile").words
-    var _words:[String] = WordList(url: "http://facultypages.morris.umn.edu/~lamberty/research/sightWords.json").words
+    var _words:[String] = WordList(url: "https://teacherwordriver.herokuapp.com/api/tile").words
+    //var _words:[String] = WordList(url: "http://facultypages.morris.umn.edu/~lamberty/research/sightWords.json").words
     //var _words:[String] = WordList(url: "http://google").words
     
     override func viewDidLoad() {
