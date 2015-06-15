7734f894ac06bf16cd2c99d31fe09c7786e1a053
Administrator <admin@x-146-57-20-28.uofm-secure.wireless.umn.edu>
2015-06-15 10:17:29 -0500

    Reading from remote JSON file in Word River to get the right words

--- MultiViews/wordList.swift
+++ MultiViews/wordList.swift
@@ -21,12 +21,47 @@ class WordList {
     }
     
     init(url: String){
-        if let dictionary = Dictionary<String, AnyObject>.loadJSONFromWeb(url) {
-            if let wordsArray: AnyObject = dictionary["words"] {
-                words = wordsArray as! [String]
+        let nsurl = NSURL(string: url)
+        var error: NSError?
+        
+        //the data seems to be not valid json
+        let data: NSData = NSData(contentsOfURL: nsurl!)!
+        println("The data is: \(data)")
+        
+        //but, we can make JSON out of the stuff that is returned when we ask for JSONSerialization on that data
+        if let dictionary: AnyObject = NSJSONSerialization.JSONObjectWithData(data,
+            options: NSJSONReadingOptions(), error: &error){
+                let somestuff = JSON(dictionary)
+                println("Some stuff is: \(somestuff)")
+                if let name = somestuff[0]["name"].string {
+                    println("The safer way: \(name)")
+                }
+        } else {
+            //and our 4th json file is not valid json, 
+            //so this is a nice way to test that this error will be triggered in such a case
+            println("The file at '\(url)' is not valid JSON, error: \(error!)")
+        }
+        
+        //or, we can try it this way, which seems less safe, but i'm not sure
+        let thestuff = JSON(data: NSData(contentsOfURL: nsurl!)!)
+        println("The stuff is: \(thestuff)")
+        
+        if let name = thestuff[0]["name"].string {
+            println("The less safe way: \(name)")
+        }
+        
+        var someWords = [String]();
+        var thecount = thestuff.count;
+        for index in 0...thecount-1 {
+            if let name = thestuff[index]["name"].string {
+                someWords.append(name)
+                println("The less safe way: \(name)")
             }
         }
         
+        println("There are \(thecount) tiles available in this collection")
+        
+        words = someWords
     }
     
     init(arr: [[String]]!) {
