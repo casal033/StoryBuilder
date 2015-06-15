e02380dea71b6cd1de098f760d204755988ad399
Administrator <admin@x-146-57-27-205.mrs.umn.edu>
2015-06-15 18:21:31 -0500

    to get Maggie's changes

--- MultiViews/GameViewController.swift
+++ MultiViews/GameViewController.swift
@@ -220,9 +220,9 @@ class GameViewController: UIViewController {
         let dataGrabber = WordList(url: "https://teacherwordriver.herokuapp.com/api/students");
         let _categoriesIDs:Array<JSON> = dataGrabber.getStudentContextIDs("https://teacherwordriver.herokuapp.com/api/students");
         var _looseTilesIDs:Array<JSON> = dataGrabber.getStudentLooseTilesIDs("https://teacherwordriver.herokuapp.com/api/students")
-        
-        var _categories:Dictionary<String, String> = WordList(urlCategories: "https://teacherwordriver.herokuapp.com/api/categories").category
-        var _tiles:Dictionary<String, Array<String>> = WordList(urlTiles: "https://teacherwordriver.herokuapp.com/api/tile").tiles
+        var _category:Dictionary<String, String> = WordList(urlCategories: "https://teacherwordriver.herokuapp.com/api/categories").category
+        var _tiles:Dictionary<String, Dictionary<String, AnyObject>> = WordList(urlTiles: "https://teacherwordriver.herokuapp.com/api/tile").tiles
+        var _categories:Dictionary<String, Array<JSON>> = WordList(urlTiles: "https://teacherwordriver.herokuapp.com/api/tile").categories
     }
     
     var _words:[String] = WordList(url: "https://teacherwordriver.herokuapp.com/api/tile").words;
